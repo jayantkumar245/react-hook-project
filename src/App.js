41a9879c10bceb59aa7fa6322c3e3dd1bfb97502
jayantkumar245 <100698593+jayantkumar245@users.noreply.github.com>
2022-09-12 09:02:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
 import React from 'react';
 import './App.css';
-import UseEffects1 from "./component/useEffect/useEffects1"
-import UseEffect2 from "./component/useEffect/useEffect2"
-import UseEffectAPI from "./component/useEffect/useEffectAPI"
-import Uncontrolled from "./component/forms/uncontrolled";
-import UseReducer from "./component/useReducer/useReducer";
-import ComA from "./component/useContext/ComA";
-import Test from "./component/useEffect/github/test";
 import Home from "./component/myapp/home";
 
 const App = () => {
 
-  // console.log(useState('thapa technical'));
+  // console.log(useState('Jayant Kumar'));
   // let myfirstVal = useState('thapa')[0];
   // console.log(myfirstVal);
 
   // var val = 'thapa technical';
 
-  // const [myName, setMyName] = useState('thapa techncial Subs');
+  // const [myName, setMyName] = useState('Jayant Kumar');
 
   // const changeName = () => {
-  //   // val = "vinod thapa";
+  //   // val = "Jayant Kuar";
   //   // console.log(val);
   //   let val = myName;
 
     // (val === 'thapa techncial Subs') ?
-    //   setMyName('vinod thapa') : setMyName('thapa techncial Subs');
+    //   setMyName('Jayant Kumar') : setMyName('Welcome to My Website');
   
   //   console.log(myName);
   return (<Home />)
